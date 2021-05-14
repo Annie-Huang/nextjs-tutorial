b0106809189a91a77e43e3bfb2d7b1ce232924a6
Annie Huang <annie.hn@gmail.com>
2021-05-15 09:34:08 +1000

    Create Navbar.js and Footer.js and add it into home page.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
@@ -12,6 +15,7 @@ export default function Home() {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
         autem debitis dolor error eum fuga itaque minima sapiente vel.
       </p>
+      <Footer />
     </div>
   );
 }
