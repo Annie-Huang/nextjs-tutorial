6485a30156c51f5bf567a3f6302470025605012d
Annie Huang <annie.hn@gmail.com>
2021-05-16 09:39:22 +1000

    Create Layout.js to contain the <Navbar> and <Footer>. And wrapped it around the <Component> in _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
@@ -19,7 +16,6 @@ export default function Home() {
       <Link href='/ninjas'>
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
