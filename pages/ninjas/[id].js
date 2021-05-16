72d6fb205dc39a673dab72f779b2947a1b35e336
Annie Huang <annie.hn@gmail.com>
2021-05-16 16:01:31 +1000

    getStaticPaths + getStaticPaths to retrieve data for every ninja.

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -16,6 +16,24 @@ export const getStaticPaths = async () => {
   };
 };
 
+/*
+   Next is going to run this function for every ninja details page that we need to generate
+   and it bases that on this paths thing line 14. So if we return 10 objects with 10 different ids
+   in this path property, it will run this function (getStaticPaths) essentially 10 times
+   so we can make 10 different fetch requests for different items. Now we get access to the parameter we need each time around on a context object
+   that we automatically accept as an argument in this function so the id of each item we need to get is going to be attached
+   to this context object so const id is equal to context.param.id
+ */
+export const getStaticPaths = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
 const Details = () => {
   return (
     <div>
