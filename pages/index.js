5e109232b7e950d1f524b8bed0a672410d07d67b
Annie Huang <annie.hn@gmail.com>
2021-05-16 11:01:06 +1000

    Copy Home.module.css across and update the className in pages/index.js file

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
         autem debitis dolor error eum fuga itaque minima sapiente vel.
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
         autem debitis dolor error eum fuga itaque minima sapiente vel.
       </p>
       <Link href='/ninjas'>
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   );
