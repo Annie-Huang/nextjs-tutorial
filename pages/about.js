c82dd1ef04e57288ebb0c471966ada7efb37692c
Annie Huang <annie.hn@gmail.com>
2021-05-16 11:50:38 +1000

    Use import Head from 'next/head'; to add <title> and <meta> info for each page.

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import Head from 'next/head';
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
-        autem debitis dolor error eum fuga itaque minima sapiente vel.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
-        autem debitis dolor error eum fuga itaque minima sapiente vel.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
+          autem debitis dolor error eum fuga itaque minima sapiente vel.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
+          autem debitis dolor error eum fuga itaque minima sapiente vel.
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
-        autem debitis dolor error eum fuga itaque minima sapiente vel.
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
-        autem debitis dolor error eum fuga itaque minima sapiente vel.
-      </p>
-      <Link href='/ninjas'>
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
+          autem debitis dolor error eum fuga itaque minima sapiente vel.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque
+          autem debitis dolor error eum fuga itaque minima sapiente vel.
+        </p>
+        <Link href='/ninjas'>
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
